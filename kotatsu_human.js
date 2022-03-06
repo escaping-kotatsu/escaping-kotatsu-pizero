d156336023cfd3157d87cf5916849cca4971440f
Motone Adachi <adachi_motone@cyberagent.co.jp>
2022-03-06 16:12:06 +0900

    wip: link human sensor to api

--- kotatsu_human.js
+++ kotatsu_human.js
@@ -1,4 +1,7 @@
+'use strict';
+
 import {requestGPIOAccess} from "./node_modules/node-web-gpio/dist/index.js"; // WebGPIO を使えるようにするためのライブラリをインポート
+const axios = require('axios');
 const sleep = msec => new Promise(resolve => setTimeout(resolve, msec)); // sleep 関数を定義
 
 human();
@@ -22,9 +25,10 @@ async function human(){
 	port.write(0);
 	console.log("START");
 
+	var h = 0; //API連携用 センサー状態
+
   while (true) {
 	  var sw = await dPort.read();
-	  var h = 0; //API連携用 センサー状態
 	  console.log(sw);
 	  if (sw == 1) {
 			port.write(1); // LEDを点灯
@@ -41,3 +45,23 @@ async function human(){
 		}
 	}
 }
+
+axios
+  .get('https://kotatsu-api.3n38.app/iot/kotatsu/1/status')
+  .then(res => {
+		const pullTimerMax =  parseInt(res.data.pull_timer);
+		let countM = 0;
+
+    (async () => {
+    	await sleep(60000);
+    	if (++countM === pullTimerMax) {
+				countM = 0;
+				const result = await axios.post('https://kotatsu-api.3n38.app/login', { name: process.env.USER_NAME, pass: process.env.USER_PASS });
+				const session = result.headers['set-cookie']['session'];
+				await axios.post('https://kotatsu-api.3n38.app/api/user/pulling/toggle/1', {
+					headers: { 'set-cookie': { session } }
+				});
+				// コタツコンソールの今直ぐひっぱる止めるAPI呼び出し 注: 当行コード動作未確認
+			}
+    })();
+	});
